f35e59cbfd56d9004dfb4687112513ce50fad990
hady-dev <hadikabalan2001@gmail.com>
2020-10-01 18:01:38 -0400

    Added Camera Face Detection

--- face_recog.py
+++ face_recog.py
@@ -2,26 +2,32 @@ import cv2
 import sys
 
 cascPath = "haarcascade_frontalface_default.xml"
-imagePath = "image1.jpg"
 
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+video_capture = cv2.VideoCapture(0)
 
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30),
-    #flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-)
+while True:
+	ret, frame = video_capture.read()
+	
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-print ("Found {0} faces!".format(len(faces)))
+	faces = faceCascade.detectMultiScale(
+	    gray,
+	    scaleFactor=1.1,
+	    minNeighbors=5,
+	    minSize=(30, 30),
+	    #flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+	)
+	
+	# Draw a rectangle around the faces
+	for (x, y, w, h) in faces:
+	    cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+	cv2.imshow('Video', frame)
+	
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+video_capture.release()
+cv2.destroyAllWindows()
